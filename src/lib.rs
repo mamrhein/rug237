c333f8c507a9e612d85365e9fe804dfed5f8192b
Michael Amrhein <michael@adrhinum.de>
2023-01-22 18:38:33 +0100

    Changed impl of Display; added fn rnd_from_exp_range.

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,19 @@
 // $Source$
 // $Revision$
 
+use rand::prelude::*;
 use rug::float::{ParseFloatError, Round};
-use rug::{Float, Integer};
+use rug::ops::Pow;
+use rug::{Assign, Float, Integer};
 use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
+use std::ops::RangeInclusive;
 use std::str::FromStr;
 
-const P: u32 = 237;
-const EMAX: i32 = 262143;
-const EMIN: i32 = 1 - EMAX;
-const MIN_EXP_SUBNORMAL: i32 = EMIN - P as i32 + 1;
-// const MIN_EXP_CUTOFF: i32 = MIN_EXP_SUBNORMAL - P as i32;
+pub const P: u32 = 237;
+pub const EMAX: i32 = 262143;
+pub const EMIN: i32 = 1 - EMAX;
+pub const MIN_EXP_SUBNORMAL: i32 = EMIN - P as i32 + 1;
 
 pub struct FP237 {
     pub(crate) f: Float,
@@ -62,6 +64,25 @@ impl FP237 {
             _ => panic!("Value is NaN or infinite."),
         }
     }
+
+    pub fn rnd_from_exp_range(exp_range: &RangeInclusive<i32>) -> Self {
+        let mut rng = thread_rng();
+        let s = rng.gen_range(0..=1_u32);
+        let t: i32 = rng.gen_range(exp_range.clone());
+        let h = rng.gen_range(0..=P - 128);
+        let l = rng.gen_range(0..=u128::MAX);
+        let mut c = (Integer::from(h) << 128) + l;
+        if t >= EMIN {
+            c += Integer::from(1) << (P - 1);
+        }
+        let mut p = Float::new(256);
+        p.assign(Float::i_exp(2, t));
+        let (mut f, o) = Float::with_val_round(P, &c * &p, Round::Nearest);
+        if s == 1 {
+            f = -f;
+        }
+        Self { f, o }
+    }
 }
 
 impl FromStr for FP237 {
@@ -82,12 +103,33 @@ impl FromStr for FP237 {
 
 impl Display for FP237 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.f.fmt(f)
+        if self.f.is_integer() {
+            let mut i = self.f.to_integer().unwrap();
+            let mut s = i.to_string();
+            let n = s.len() as u32;
+            if n > 72 {
+                let d = Integer::from(10).pow(n - 72);
+                let mut t = Integer::new();
+                t.assign(&d >> 1);
+                let qr = i.div_rem_ref(&d);
+                let mut q = Integer::new();
+                let mut r = Integer::new();
+                (&mut q, &mut r).assign(qr);
+                if r > t || r == t && q.is_odd() {
+                    q += 1;
+                }
+                i.assign(q * &d);
+                s = i.to_string();
+            }
+            f.write_str(&s)
+        } else {
+            self.f.fmt(f)
+        }
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod decode_tests {
     use super::*;
 
     #[test]
@@ -102,7 +144,10 @@ mod tests {
     fn test_min_pos_subnormal() {
         let e = Float::with_val(P, Float::parse("-262378.").unwrap());
         let t = e.exp2();
-        let f = FP237 { f: t.clone(), o: Ordering::Equal };
+        let f = FP237 {
+            f: t.clone(),
+            o: Ordering::Equal,
+        };
         assert_eq!(f.decode(), (0, -262378, (0, 1)));
     }
 
@@ -137,9 +182,15 @@ mod tests {
         let e = Float::with_val(P, Float::parse("261907.").unwrap());
         let b = e.exp2();
         let t = a - b;
-        let f = FP237 { f: t.clone(), o: Ordering::Equal };
+        let f = FP237 {
+            f: t.clone(),
+            o: Ordering::Equal,
+        };
         println!("{f}");
-        assert_eq!(f.decode(), (0, 261907, ((1 << (237 - 128)) - 1, u128::MAX)));
+        assert_eq!(
+            f.decode(),
+            (0, 261907, ((1 << (237 - 128)) - 1, u128::MAX))
+        );
     }
 
     #[test]
@@ -148,5 +199,21 @@ mod tests {
         let f = FP237::from_str(s).unwrap();
         println!("{}", f);
         assert_eq!(f.decode(), (0, -3, (0, 141)));
+    }
+}
+
+#[cfg(test)]
+mod rnd_tests {
+    use super::*;
+
+    #[test]
+    fn test_normal_lt1() {
+        let exp_range: RangeInclusive<i32> = -304..=-236;
+        let f = FP237::rnd_from_exp_range(&exp_range);
+        assert_eq!(f.f.prec(), P);
+        let (s, e, (h, _)) = f.decode();
+        assert!(s == 0 || s == 1);
+        assert!(exp_range.contains(&e));
+        assert!(h.leading_zeros() >= 256 - P);
     }
 }
