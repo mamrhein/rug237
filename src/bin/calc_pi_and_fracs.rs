c2c4dd70cccc3712510b2542ea8521d2582fa85f
Michael Amrhein <michael@adrhinum.de>
2023-10-31 13:34:08 +0100

    Added calculation of τ = 2π.

--- src/bin/calc_pi_and_fracs.rs
+++ src/bin/calc_pi_and_fracs.rs
@@ -13,8 +13,22 @@ const P: u32 = 249;
 
 fn main() {
     let b: Integer = Integer::from(1) << 128;
+    let const_pi = Float::with_val(P + 5, rug::float::Constant::Pi);
 
-    let pi = Float::with_val(P + 1, rug::float::Constant::Pi);
+    let tau = Float::with_val(P + 2, 2 * const_pi.clone());
+    let (m, e) = tau.to_integer_exp().unwrap();
+    // println!("{e} {m:064x}");
+    assert_eq!(e, -248);
+    let (q, r) = &m.div_rem(b.clone());
+    let hi: u128 = q.to_u128_wrapping();
+    let lo: u128 = r.to_u128_wrapping();
+    assert_eq!(hi.leading_zeros(), 5);
+    println!("    // {tau}");
+    println!(
+        "    pub(crate) const TAU: FP248 = FP248 {{\n sign: 0,\n signif: \
+         u256::new(0x{hi:>032x}, 0x{lo:>032x}),\n }};"
+    );
+    let pi = Float::with_val(P + 1, const_pi.clone());
     let (m, e) = pi.to_integer_exp().unwrap();
     // println!("{e} {m:064x}");
     assert_eq!(e, -248);
@@ -27,7 +41,7 @@ fn main() {
         "    pub(crate) const PI: FP248 = FP248 {{\n sign: 0,\n signif: \
          u256::new(0x{hi:>032x}, 0x{lo:>032x}),\n }};"
     );
-    let frac_pi_2 = Float::with_val(P, pi / 2);
+    let frac_pi_2 = Float::with_val(P, const_pi.clone() / 2);
     let (m, e) = frac_pi_2.to_integer_exp().unwrap();
     // println!("{e} {m:064x}");
     assert_eq!(e, -248);
